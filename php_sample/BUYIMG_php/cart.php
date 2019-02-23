28b0acf43b3f696d47c78d8079a772ff7fe60719
wswsaber <wswsaber@yahoo.co.jp>
2019-02-23 15:27:31 +0900

    php_sampleにBUYING_php追加
    DBも更新しました

---
+++ php_sample/BUYIMG_php/cart.php
@@ -0,0 +1,97 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="content-style-type" content="text/css"/>
+
+		<title>カートページ|BUYIMG</title>
+		<link href="./css/bootstrap.css" rel="stylesheet" type="text/css"/>
+
+		<link href="https://fonts.googleapis.com/css?family=Charmonman" rel="stylesheet">
+		<link href="./css/テンプレート(検索フォーム無).css" rel="stylesheet" type="text/css" />
+
+	</head>
+	<body>
+		<!-- header -->
+			<div class="header">
+				<a href="index.html"><img src="./img/common/logo_white.png" class="header_logo"></a>
+				<div class="header_buttons">
+					<a href="blank"><img src="./img/common/header_button1.png" class="header_button1"></a>
+					<a href="./cart.html"><img src="./img/common/header_button2.png" class="header_button2"></a>
+				</div>
+			</div>
+		<!-- /header -->
+		<br/><br/><br/>
+		<h2>カート</h2>
+		<hr/>
+		<!--main start-->
+			<div class="container pt-0 pb-1 pl-2 pr-3">
+				<table class="table table-responsive-md table-hover table-striped">
+					<tr>
+						<td>
+							<img src ="./images/event.jpg" alt="ワイン"/>
+						</td>
+						<td>
+							<label>画像名:</label>ワイン<br/>
+							<label>単価:<label>10円
+						</td>
+						<td>
+							<button type="button" name="delete" class="btn btn-secondary btn-lg btn-block">削除</button>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<img src ="./images/human_2.jpg" alt="カメラマン"/>
+						</td>
+						<td>
+							<label>画像名:</label>カメラマン<br/>
+							<label>単価:<label>10円
+						</td>
+						<td>
+							<button type="button" name="delete" class="btn btn-secondary btn-lg btn-block">削除</button>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<img src ="./images/life.jpg" alt="coffee"/>
+						</td>
+						<td>
+							<label>画像名:</label>coffee<br/>
+							<label>単価:<label>10円
+						</td>
+						<td>
+								<button type="button" name="delete" class="btn btn-secondary btn-lg btn-block">削除</button>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<img src ="./images/nature.jpg" alt="自然"/>
+						</td>
+						<td>
+							<label>画像名:</label>自然<br/>
+							<label>単価:<label>10円
+						</td>
+						<td>
+								<button type="button" name="delete"  class="btn btn-secondary btn-lg btn-block">削除</button>
+						</td>
+					</tr>
+				</table>
+				<div class="table table-responsive-md table-hover table-striped text-right">
+					<form method="post" action="#">
+						<label><strong>合計：<span>40</span>円<strong></label>　<button type="button" name="cash" class="btn btn-secondary btn-lg">決済</button>
+					</form>
+				</div>
+			</div>
+
+			<br/>	<br/><br/><br/>
+		<!--main end-->
+
+		<!-- footer -->
+        	<div id="footer">
+				Copyright © RAIN. All Rights Reserved.
+			</div>
+	     </div>
+		<!-- /footer -->
+	</body>
+</html>
---
+++ php_sample/BUYIMG_php/preview.php
@@ -0,0 +1,117 @@
+<!--
+	表示する画像は「横320px」で統一すること！！
+
+-->
+
+<?php
+	$con=mysqli_connect("localhost","root","")or die("失敗");
+	mysqli_set_charset($con,"utf8");
+	mysqli_select_db($con,"rain_site");
+	if(isset($_POST["seaech_word"]))
+	{
+		//とりあえずIDと画像だけを取得して、preview_detailのページでその他の情報を取得させる
+		$sql=" select product_id,product_path from product_master where product_name like'%{$_POST["seaech_word"]}' OR product_introduction like'%{$_POST["seaech_word"]}' OR big_category like'%{$_POST["seaech_word"]}'";
+	}
+	else
+	{
+		$sql=" select product_id,product_path from product_master where big_category='{$_GET["big_category"]}'";
+	}
+	$result=mysqli_query($con,$sql);
+	$list = array();
+	while ($row = mysqli_fetch_assoc($result)) {
+		$list[] = $row;
+	}
+	//db_close($con);
+?>
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="content-style-type" content="text/css"/>
+
+		<title>素材一覧|BUYIMG</title>
+
+		<link href="./css/preview.css" rel="stylesheet" type="text/css" />
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="js/imagesloaded.pkgd.min.js"></script>
+		<script src="js/masonry.pkgd.min.js"></script>
+		<!--
+		<script src="js/jquery.matchHeight.js"></script>
+		-->
+		<script>
+			$(window).load(function() {
+				$('.grid').masonry({
+    				itemSelector: '.grid-item',
+    				isFitWidth: true
+ 				});
+    		});
+
+/*
+			$(window).load(function() {
+		    	$('.grid').masonry({
+			   	 });
+			});
+ 		 	var $grid = jQuery('.grid').imagesLoaded(function(){
+ 				 $grid.masonry({
+    			itemSelector: '.grid-item'
+ 			 	});
+			});
+*/
+		</script>
+	</head>
+	<body>
+		<!-- header -->
+			<div class="header">
+				<a href="index.html"><img src="./img/common/logo_white.png" class="header_logo"></a>
+				<!-- 検索フォーム -->
+				<form method="post" action="./preview.php" class="search_container">
+					<input type="text" size="25" placeholder="キーワード検索" name="seaech_word">
+	  				<input type="submit" value="">
+				</form>
+
+				<div class="header_buttons">
+					<a href="blank"><img src="./img/common/header_button1.png" class="header_button1"></a>
+					<a href="./cart.php"><img src="./img/common/header_button2.png" class="header_button2"></a>
+				</div>
+			</div>
+		<!-- /header -->
+		<h3><?php echo $_GET["big_category"]; ?>の写真素材　xx件</h3>
+
+		<hr>
+		<a href="" class="order_link">▼新着順</a>
+
+
+		<?php foreach ($list as $row) {?>
+			<form method="post" action="./preview_detail.php">
+				<input type="hidden" name="product_id" value="<?php echo $row["product_id"];?>">
+				<input type="image" src="<?php print $row["product_path"]; ?>" alt="商品" width=200px height="200px">
+			</form>
+			<?php } ?>
+
+
+		<!--
+		<div class="grid">
+			<div class="grid-item item-1" class="item"><span><img src="img/preview/1.jpg"></span></div>
+			<div class="grid-item item-2" class="item"><span><img src="img/preview/2.jpg"></span></div>
+			<div class="grid-item item-3" class="item"><span><img src="img/preview/3.jpg"></span></div>
+			<div class="grid-item item-1" class="item"><span><img src="img/preview/1.jpg"></span></div>
+			<div class="grid-item item-2" class="item"><span><img src="img/preview/2.jpg"></span></div>
+			<div class="grid-item item-3" class="item"><span><img src="img/preview/3.jpg"></span></div>
+			<div class="grid-item item-1" class="item"><span><img src="img/preview/1.jpg"></span></div>
+			<div class="grid-item item-2" class="item"><span><img src="img/preview/2.jpg"></span></div>
+			<div class="grid-item item-3" class="item"><span><img src="img/preview/3.jpg"></span></div>
+			<div class="grid-item item-1" class="item"><span><img src="img/preview/1.jpg"></span></div>
+			<div class="grid-item item-2" class="item"><span><img src="img/preview/2.jpg"></span></div>
+			<div class="grid-item item-3" class="item"><span><img src="img/preview/3.jpg"></span></div>
+
+		</div>
+		-->
+		<!-- footer -->
+         <div id="footer">
+                  Copyright © RAIN. All Rights Reserved.
+         </div>
+		<!-- /footer -->
+	</body>
+</html>
