6ec79901646332117b4f4bc5d69c21fe2d426573
feifei123wyz <feifei1988wyz@gmail.com>
2019-02-28 14:53:37 +0900

    cart.phpページの変更

--- BUYIMG_php/cart.php
+++ BUYIMG_php/cart.php
@@ -14,6 +14,8 @@ session_start();
 		<link href="https://fonts.googleapis.com/css?family=Charmonman" rel="stylesheet">
 		<link href="./css/テンプレート(検索フォーム無).css" rel="stylesheet" type="text/css" />
 
+
+
 	</head>
 	<body>
 		<!-- header -->
@@ -25,115 +27,86 @@ session_start();
 				</div>
 			</div>
 		<!-- /header -->
-		<br/><br/><br/>
-		<h2>カート</h2>
-		<hr/>
-		<!--main start-->
 
-			<div class="container pt-0 pb-1 pl-2 pr-3">
-				<table class="table table-responsive-md table-hover table-striped">
-					<?php
-					//print_r($_SESSION["cart"]);
-					if(isset($_SESSION["cart"])){
-						$con=mysqli_connect("localhost","root","")or die("失敗");
-						mysqli_set_charset($con,"utf8");
 						mysqli_select_db($con,"rain_site");
-						$sum = 0;
-						$cnt=0;
-						while(current($_SESSION["cart"]))
-						{
-							$key = key($_SESSION["cart"]);
-							$sql=" select * from product_master where product_id='{$key}'";
-							$result=mysqli_query($con,$sql);
-							$row=mysqli_fetch_array($result);
-						?>
-					<tr>
-						<td>
-							<img src="<?php print $row["product_path"]; ?>" alt="ワイン" class="figure-img"/>
-						</td>
-						<td>
-							<label>画像名:</label><?php print $row["product_name"]; ?><br/>
-							<label>単価:<label><?php print $row["price"]; ?>円
-						</td>
-						<td>
-							<form method="post" action="./remove_cart.php">
-								<input type="hidden" name="product_id" value="<?php print $row["product_id"]; ?>">
-								<input type="submit" value="削除" class="btn btn-secondary btn-lg btn-block"><br><br>
-							</form>
-							<!--<button type="button" name="delete" class="btn btn-secondary btn-lg btn-block">削除</button>-->
-						</td>
-					</tr>
-					<?php
-						$sum += $row["price"];
-						$cnt++;
-						next($_SESSION["cart"]);
+		<!--main start-->
+		<div class="container">
+			<br/><br/><br/>
+			<h2>カート</h2><hr/>
+
+			<?php
+			//print_r($_SESSION["cart"]);
+			if(isset($_SESSION["cart"])){
+				$con=mysqli_connect("localhost","root","")or die("失敗");
+				mysqli_set_charset($con,"utf8");
+				mysqli_select_db($con,"rain_site");
+				$sum = 0;
+				$cnt=0;
+				while(current($_SESSION["cart"]))
+				{
+					$key = key($_SESSION["cart"]);
+					$sql=" select * from product_master where product_id='{$key}'";
+					$result=mysqli_query($con,$sql);
+					$row=mysqli_fetch_array($result);
+				?>
+				<div class="pt-0 pb-1 pl-2 pr-3">
+							<div>
+								<table class="table table-responsive-md table-hover table-striped">
+
+									<tr>
+										<td>
+											<img src="<?php print $row["product_path"]; ?>" alt="ワイン" class="figure-img"/>
+										</td>
+										<td>
+											<label>画像名:</label><?php print $row["product_name"]; ?><br/>
+											<label>単価:<label><?php print $row["price"]; ?>円
+										</td>
+										<td>
+											<form method="post" action="./remove_cart.php">
+												<input type="hidden" name="product_id" value="<?php print $row["product_id"]; ?>">
+												<input type="submit" value="削除" class="btn btn-secondary btn-lg btn-block"><br><br>
+											</form>
+											<!--<button type="button" name="delete" class="btn btn-secondary btn-lg btn-block">削除</button>-->
+										</td>
+									</tr>
+									<?php
+										$sum += $row["price"];
+										$cnt++;
+										next($_SESSION["cart"]);
+									}
+									?>
+								</table>
+							</div>
+								<!--決済-->
+								<div class="table table-responsive-md table-hover table-striped text-right">
+									<form method="post" action="#">
+										<label><strong>合計：<span><?php print $sum; ?></span>円<strong></label>　<button type="button" name="cash" class="btn btn-secondary btn-lg">決済</button>
+									</form>
+								</div>
+				</div>
+				<?php
+					}
+					else
+					{
+					?>
+						<div class="text-center">
+							<br/>
+							<br/>
+							<br/>
+							<h2>カートが空です</h2>
+							<a href="index.html">topページ戻ります</a>
+						</div>
+				<?php
 					}
 					?>
-					<!--
-					<tr>
-						<td>
-							<img src ="./images/human_2.jpg" alt="カメラマン"/>
-						</td>
-						<td>
-							<label>画像名:</label>カメラマン<br/>
-							<label>単価:<label>10円
-						</td>
-						<td>
-							<button type="button" name="delete" class="btn btn-secondary btn-lg btn-block">削除</button>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<img src ="./images/life.jpg" alt="coffee"/>
-						</td>
-						<td>
-							<label>画像名:</label>coffee<br/>
-							<label>単価:<label>10円
-						</td>
-						<td>
-								<button type="button" name="delete" class="btn btn-secondary btn-lg btn-block">削除</button>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<img src ="./images/nature.jpg" alt="自然"/>
-						</td>
-						<td>
-							<label>画像名:</label>自然<br/>
-							<label>単価:<label>10円
-						</td>
-						<td>
-								<button type="button" name="delete"  class="btn btn-secondary btn-lg btn-block">削除</button>
-						</td>
-					</tr>
-				-->
-				</table>
-				<div class="table table-responsive-md table-hover table-striped text-right">
-					<form method="post" action="#">
-						<label><strong>合計：<span><?php print $sum; ?></span>円<strong></label>　<button type="button" name="cash" class="btn btn-secondary btn-lg">決済</button>
-					</form>
-				</div>
-			</div>
-
-			<br/>	<br/><br/><br/>
-			<?php
-				}
-				else
-				{
-				?>
-					カートが空です<br>
-					<a href="index.html">INDEX</a>
-			<?php
-				}
-				?>
+					<div class="clearfix"></div>
+	</div>
 
 		<!--main end-->
-
 		<!-- footer -->
-        	<div id="footer">
-				Copyright © RAIN. All Rights Reserved.
-			</div>
-	     </div>
+		<div id="footer">
+			Copyright © RAIN. All Rights Reserved.
+		</div>
 		<!-- /footer -->
 	</body>
 </html>
